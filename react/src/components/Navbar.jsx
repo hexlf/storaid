65e8f81701e6c0a000c28c23a9a83655cf4600e7
ZelFS <86790952+hexlf@users.noreply.github.com>
2025-11-01 17:24:06 +0100

    setting colors and testing

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,27 +1,9 @@
 function Navbar() {
   return (
     <div>
-      <nav classname="navbar navbar-expand-lg bg-body-tertiary">
-        <div classname="container-fluid">
-          <a classname="navbar-brand" href="#">Navbar</a>
-          <button classname="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span classname="navbar-toggler-icon"></span>
-          </button>
-          <div classname="collapse navbar-collapse" id="navbarNav">
-            <ul classname="navbar-nav">
-              <li classname="nav-item">
-                <a classname="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li classname="nav-item">
-                <a classname="nav-link" href="#">Features</a>
-              </li>
-              <li classname="nav-item">
-                <a classname="nav-link" href="#">Pricing</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <button className="test">
+      test
+      </button>
     </div>
   )
 }

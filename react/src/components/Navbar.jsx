cd2dc4cd9660654faf9830f1d5296cd0d5f32e69
ZelFS <86790952+hexlf@users.noreply.github.com>
2025-11-03 13:25:29 +0100

    broken css can edit css all links to white but not specifs ones

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -3,31 +3,32 @@ function Navbar() {
     <div>
       <div className="Navbarcontainer">
         <div className="NeedsToBeDarkGreen">
-          <div>
-            <img className="smallimg" src="src/img/phone.png" alt="phone number" />
-            <a href="X">+46 8 123 122 44</a>
-            <img className="smallimg" src="src/img/mail.png" alt="gmail" />
-            <a href="X">contact@domain.com</a>
+          <div className="phone-mail">
+            <div>
+              <img className="smallimg" src="src/img/phone.png" alt="phone number" />
+              <a href="X">+46 8 123 122 44</a>
+            </div>
+            <div>
+              <img className="smallimg" src="src/img/mail.png" alt="gmail" />
+              <a href="X">contact@domain.com</a>
+            </div>
           </div>
-          <div>
-            <a href=""><img src="" alt="" /></a>
-            <a href=""><img src="" alt="" /></a>
-            <a href=""><img src="" alt="" /></a>
-            <a href=""><img src="" alt="" /></a>
+          <div className="imgcontainer">
+            <img className="imgneed30" src="src/img/facebook.png" alt="" />
+            <img className="imgneed30" src="src/img/X.png" alt="" />
+            <img className="imgneed30" src="src/img/instagram.png" alt="" />
+            <img className="imgneed30" src="src/img/youtube.png" alt="" />
           </div>
         </div>
+        <div className="NeedToBeWhite">
+          <a href="X"><img src="src/img/Logo.png" alt="Logo" /></a>
+          <a href="">Home</a>
+          <a href="">About Us</a>
+          <a href="">Services</a>
+          <a href="">Contact Us</a>
+          <a href="booking.jsx" className="NeedToBeYelow">Book Now</a>
+        </div>
       </div>
-      <div className="NeedToBeWhite">
-        <a href="X"><img src="src/img/Logo.png" alt="Logo" /></a>
-        <a href="">Home</a>
-        <a href="">About Us</a>
-        <a href="">Services</a>
-        <a href="">Contact Us</a>
-        <a href="booking.html" className="NeedToBeYelow">Book Now</a>
-      </div>
-      <button  className="test">
-      test
-      </button>
     </div>
   )
 }

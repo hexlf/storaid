2fa9f5d58bf7c4ae95929074b430142683659182
ZelFS <86790952+hexlf@users.noreply.github.com>
2025-11-09 20:16:48 +0100

    pricing Done but cant change img size on the list dots

--- react/src/components/Pricing.jsx
+++ react/src/components/Pricing.jsx
@@ -2,8 +2,119 @@ import React from 'react'
 
 function Pricing() {
   return (
-    <div>
-        Pricing
+    <div className='Pricing'>
+        <h4 className='NeedsUrbanFont NeedToBeYelow'>Pricing Plan</h4>
+        <h3 className='NeedsUrbanFont NeedsToBeDarkGreen'>Find the Perfect Plan for Your Storage Needs</h3>
+        <div className='Cards'>
+          <div className='Card-container'>
+            <div className='card-toptext'>
+              <h5 className='NeedsUrbanFont'>Small Unit</h5>
+              <div className='price'>
+                <h3>
+                  50
+                </h3>
+                <h6 className='NeedToBeYelow NeedsUrbanFont'>
+                  $
+                </h6>
+                <h6>
+                  /month
+                </h6>
+              </div>
+              <p className='NeedsUrbanFont'>
+                Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.
+              </p>
+            </div>
+            <ul>
+              <li>Nam nec ipsum in dolor</li>
+              <li>Fusce nec ligula ut arcu</li>
+              <li>Aliquam pulvinar arcu in</li>
+              <li>Duis gravida enim porta</li>
+              <li>Etiam eget libero non ligula</li>
+            </ul>
+            <a className='NeedsUrbanFont NeedToBeYelowbtn' href="X">Rent Now</a>
+          </div>
+          <div className='Card-container'>
+            <div className='card-toptext'>
+              <h5 className='NeedsUrbanFont'>Small Unit</h5>
+              <div className='price'>
+                <h3>
+                  100
+                </h3>
+                <h6 className='NeedToBeYelow NeedsUrbanFont'>
+                  $
+                </h6>
+                <h6>
+                  /month
+                </h6>
+              </div>
+              <p className='NeedsUrbanFont'>
+                Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.
+              </p>
+            </div>
+            <ul>
+              <li>Nam nec ipsum in dolor</li>
+              <li>Fusce nec ligula ut arcu</li>
+              <li>Aliquam pulvinar arcu in</li>
+              <li>Duis gravida enim porta</li>
+              <li>Etiam eget libero non ligula</li>
+            </ul>
+            <a className='NeedsUrbanFont NeedToBeYelowbtn' href="X">Rent Now</a>
+          </div>
+          <div className='Card-container'>
+            <div className='card-toptext'>
+              <h5 className='NeedsUrbanFont'>Small Unit</h5>
+              <div className='price'>
+                <h3>
+                  150
+                </h3>
+                <h6 className='NeedToBeYelow NeedsUrbanFont'>
+                  $
+                </h6>
+                <h6>
+                  /month
+                </h6>
+              </div>
+              <p className='NeedsUrbanFont'>
+                Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.
+              </p>
+            </div>
+            <ul>
+              <li>Nam nec ipsum in dolor</li>
+              <li>Fusce nec ligula ut arcu</li>
+              <li>Aliquam pulvinar arcu in</li>
+              <li>Duis gravida enim porta</li>
+              <li>Etiam eget libero non ligula</li>
+            </ul>
+            <a className='NeedsUrbanFont NeedToBeYelowbtn' href="X">Rent Now</a>
+          </div>
+          <div className='Card-container'>
+            <div className='card-toptext'>
+              <h5 className='NeedsUrbanFont'>Small Unit</h5>
+              <div className='price'>
+                <h3>
+                  200
+                </h3>
+                <h6 className='NeedToBeYelow NeedsUrbanFont'>
+                  $
+                </h6>
+                <h6>
+                  /month
+                </h6>
+              </div>
+              <p className='NeedsUrbanFont'>
+                Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.
+              </p>
+            </div>
+            <ul>
+              <li>Nam nec ipsum in dolor</li>
+              <li>Fusce nec ligula ut arcu</li>
+              <li>Aliquam pulvinar arcu in</li>
+              <li>Duis gravida enim porta</li>
+              <li>Etiam eget libero non ligula</li>
+            </ul>
+            <a className='NeedsUrbanFont NeedToBeYelowbtn' href="X">Rent Now</a>
+          </div>
+        </div>
     </div>
   )
 }

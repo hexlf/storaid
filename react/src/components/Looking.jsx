3db5cb9a347b28ea9e909abdf8a25b3f13ab620f
ZelFS <86790952+hexlf@users.noreply.github.com>
2025-11-09 23:13:40 +0100

    Looking Done

--- react/src/components/Looking.jsx
+++ react/src/components/Looking.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Looking() {
   return (
-    <div>
-        Looking
+    <div className='Looking'>
+        <h3 className='NeedsUrbanFont'>Looking for Secure and Flexible Storage? Find Your Perfect Fit With Us.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+        <a className='NeedToBeYelowbtn btn-area NeedsUrbanFont' href="X">Book Now</a>
     </div>
   )
 }

19c406fae559fbb163cf8efa460232b57276f72d
ZelFS <86790952+hexlf@users.noreply.github.com>
2025-11-11 20:44:43 +0100

    Footer done not done with Subscirbe problems with red dot in textinput

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-        Footer
+    <div className='footer'>
+        <p>© 2025 StorAid, All rights reserved.</p>
+        <div>
+          <a href="X"><img src="src/img/footericon.png" alt="icon for link" />Terms & Conditions</a>
+          <a href="X"><img src="src/img/footericon.png" alt="icon for link" />Privacy Policy</a>
+        </div>
     </div>
   )
 }
--- react/src/components/Subscribe.jsx
+++ react/src/components/Subscribe.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function Subscribe() {
   return (
-    <div>
-        Subscribe
+    <div className='Subscribe-area'>
+      <div className='Subscribe-textside'>
+        <h3 className='NeedsUrbanFont NeedsToBeDarkGreen'>Subscribe Our Newsletter</h3>
+        <p className='NeedsToBeDarkGreen'>Subscribe to our newsletter to receive early discount offers, updates and info</p>
+      </div>
+      <div className='Subscribe-submit'> 
+        <form action="">
+          <span className='red'>*</span>
+          <input className='textinput' type="text" required placeholder="Enter your email" />
+          <button className='NeedToBeYelowbtn' type="submit">submit</button>
+        </form>
+      </div>
     </div>
   )
 }
